c73b572752be2db349820971b60a95120f396036
Adrián Ramos <adrianramosrp@gmail.com>
2020-05-20 22:49:32 +0200

    feat: implement tutorial 4
    Fill board spawning randon pieces in virtual grid

--- Scripts/grid.gd
+++ Scripts/grid.gd
@@ -7,11 +7,22 @@ export (int) var x_start;
 export (int) var y_start;
 export (int) var offset;
 
+var possible_pieces = [
+	preload("res://Scenes/yellow_piece.tscn"),
+	preload("res://Scenes/blue_piece.tscn"),
+	preload("res://Scenes/pink_piece.tscn"),
+	preload("res://Scenes/orange_piece.tscn"),
+	preload("res://Scenes/green_piece.tscn"),
+	preload("res://Scenes/light_green_piece.tscn")
+];
+
 var all_pieces = [];
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	randomize();
 	all_pieces = make_2d_array();
+	spawn_pieces();
 
 func make_2d_array():
 	var array = [];
@@ -20,6 +31,23 @@ func make_2d_array():
 		for j in height:
 			array[i].append(null);
 	return array;
+
+func spawn_pieces():
+	for i in width:
+		for j in height:
+			# choose a randon number and store it
+			var rand = floor(rand_range(0, possible_pieces.size()));
+			# instance that piece from the array
+			var piece = possible_pieces[rand].instance();
+			add_child(piece);
+			piece.position = grid_to_pixel(i, j);
+			
+
+func grid_to_pixel(column, row):
+	var new_x = x_start + offset * column;
+	var new_y = y_start + -offset * row;
+	return Vector2(new_x, new_y);
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
